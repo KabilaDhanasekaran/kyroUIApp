4d33b92c0c4909fbb1e608944b16d7373f040043
KabilaDhanasekaran <kabila1994@gmail.com>
2022-12-30 17:27:45 +0530

    Add Topbar with the required options

--- src/scenes/global/Topbar.jsx
+++ src/scenes/global/Topbar.jsx
@@ -1,5 +1,38 @@
+import { Box, Button, Avatar, Typography, Grid } from "@mui/material";
+import AddIcon from '@mui/icons-material/Add';
+import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown';
+
+const currentDate = new Date();
+
 const Topbar = () => {
-    return <div>Topbar</div>
+    return (
+    <Box display="flex" justifyContent="space-between" p={2}>
+        <Box display="inline-block" >
+            <Typography variant="h3">Good Morning, Kabila</Typography>
+            {/* use momentJs */}
+            <Typography variant="subtitle1">{currentDate.getDate() + ' , '+ currentDate.getFullYear()}</Typography>
+        </Box>
+        <Box display="flex" justifyContent="space-between">
+            <Button variant="contained" color="error" size="small" startIcon={<AddIcon />}>
+                Add Project
+            </Button>
+
+            <Button 
+                variant="text" 
+                startIcon={<Avatar sx={{ width: 32, height: 32 }}>K</Avatar>}
+                endIcon={<ArrowDropDownIcon />}>
+                <Grid container direction="column" alignItems="start" >
+                    <Grid item>
+                        <Typography variant="h3">Kabila D</Typography>
+                    </Grid>
+                    <Grid item>
+                        <Typography variant="subtitle1">Senior Consultant</Typography>
+                    </Grid>
+                </Grid>
+            </Button>
+        </Box>
+    </Box>
+    )
 }
 
 export default Topbar;
