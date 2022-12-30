11481183e20ef3074d8dfa27580206c99e701db1
KabilaDhanasekaran <kabila1994@gmail.com>
2022-12-30 18:58:56 +0530

    Add sidebar for navigation

--- src/scenes/global/Sidebar.jsx
+++ src/scenes/global/Sidebar.jsx
@@ -1,5 +1,117 @@
+import { useState } from "react";
+import { ProSidebarProvider, Menu, MenuItem } from "react-pro-sidebar";
+import { Box, IconButton, Typography } from "@mui/material";
+import HomeOutlinedIcon from "@mui/icons-material/HomeOutlined";
+import GridViewRoundedIcon from '@mui/icons-material/GridViewRounded';
+import ForumRoundedIcon from '@mui/icons-material/ForumRounded';
+import CorporateFareRoundedIcon from '@mui/icons-material/CorporateFareRounded';
+import ArticleIcon from '@mui/icons-material/Article';
+import SettingsRoundedIcon from '@mui/icons-material/SettingsRounded';
+import MenuOutlinedIcon from "@mui/icons-material/MenuOutlined";
+
+const Item = ({ title, to, icon, selected, setSelected }) => {
+  return (
+    <MenuItem
+      active={selected === title}
+      onClick={() => setSelected(title)}
+      icon={icon}
+    >
+      <Typography>{title}</Typography>
+    </MenuItem>
+  );
+};
+
 const Sidebar = () => {
-  return <div>Sidebar</div>
-}
+  const [isCollapsed, setIsCollapsed] = useState(false);
+  const [selected, setSelected] = useState("");
+
+  return (
+    <Box sx={{ width: "25%"}}>
+      <ProSidebarProvider collapsed={isCollapsed}>
+        <Menu iconShape="square">
+          {/* LOGO AND MENU ICON */}
+          <MenuItem
+            onClick={() => setIsCollapsed(!isCollapsed)}
+            icon={isCollapsed ? <MenuOutlinedIcon /> : undefined}
+            style={{
+              margin: "10px 0 20px 0",
+            }}
+          >
+            {!isCollapsed && (
+              <Box
+                display="flex"
+                justifyContent="space-between"
+                alignItems="center"
+                ml="15px"
+              >
+                <Typography variant="h4">
+                  KYRO
+                </Typography>
+                <IconButton onClick={() => setIsCollapsed(!isCollapsed)}>
+                  <MenuOutlinedIcon />
+                </IconButton>
+              </Box>
+            )}
+          </MenuItem>
+
+          <Box paddingLeft={isCollapsed ? undefined : "10%"}>
+            <Item
+              title="Home"
+              to="/"
+              icon={<HomeOutlinedIcon />}
+              selected={selected}
+              setSelected={setSelected}
+            />
+
+            <Item
+              title="Projects"
+              to="/projects"
+              icon={<ArticleIcon />}
+              selected={selected}
+              setSelected={setSelected}
+            />
+            <Item
+              title="Dashboard"
+              to="/dashboard"
+              icon={<GridViewRoundedIcon />}
+              selected={selected}
+              setSelected={setSelected}
+            />
+            <Item
+              title="Messages"
+              to="/messages"
+              icon={<ForumRoundedIcon />}
+              selected={selected}
+              setSelected={setSelected}
+            />
+
+            <Item
+              title="Documents"
+              to="/documents"
+              icon={<ArticleIcon />}
+              selected={selected}
+              setSelected={setSelected}
+            />
+            <Item
+              title="Organizations"
+              to="/organizations"
+              icon={<CorporateFareRoundedIcon />}
+              selected={selected}
+              setSelected={setSelected}
+            />
+            <Item
+              title="Settings"
+              to="/settings"
+              icon={<SettingsRoundedIcon />}
+              selected={selected}
+              setSelected={setSelected}
+            />
+
+          </Box>
+        </Menu>
+      </ProSidebarProvider>
+    </Box>
+  );
+};
 
 export default Sidebar;
